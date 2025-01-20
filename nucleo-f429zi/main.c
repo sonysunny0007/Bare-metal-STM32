3162d4b126b420a5d7801406b632ac027c42d958
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2025-01-19 23:33:27 -0700

    Adding bare metal program for nucleo-f429zi

---
+++ nucleo-f429zi/main.c
@@ -0,0 +1,43 @@
+//Author sonysunny
+
+#include "hal.h"
+
+#define BLINK_PERIOD_MS 500  // LED blinking period in millis
+#define LOG_PERIOD_MS 1000   // Info log period in millis
+
+uint32_t SystemCoreClock;  // Required by CMSIS. Holds system core cock value
+void SystemInit(void) {    // Called automatically by startup code
+  clock_init();            // Sets SystemCoreClock
+}
+
+static volatile uint64_t s_ticks;  // Milliseconds since boot
+void SysTick_Handler(void) {       // SyStick IRQ handler, triggered every 1ms
+  s_ticks++;
+}
+
+static void led_task(void) {  // Blink LED every BLINK_PERIOD_MS
+  static uint64_t timer = 0;
+  if (timer_expired(&timer, BLINK_PERIOD_MS, s_ticks)) {
+    gpio_toggle(LED_PIN);
+  }
+}
+
+static void log_task(void) {  // Print a log every LOG_PERIOD_MS
+  static uint64_t timer = 0;
+  if (timer_expired(&timer, LOG_PERIOD_MS, s_ticks)) {
+    printf("tick: %5lu, CPU %lu MHz\n", (unsigned long) s_ticks,
+           SystemCoreClock / 1000000);
+  }
+}
+
+int main(void) {
+  gpio_output(LED_PIN);
+  uart_init(UART_DEBUG, 115200);
+
+  for (;;) {
+    led_task();
+    log_task();
+  }
+
+  return 0;
+}
